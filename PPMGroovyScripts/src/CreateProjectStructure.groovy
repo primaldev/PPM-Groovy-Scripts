3d9daa880824a844066f15a482c81e48b35ee4ef
Marco van Zoest <marco@primaldev.nl>
2014-10-23 14:23:25 +0200

    Started with document templates

--- PPMGroovyScripts/src/CreateProjectStructure.groovy
+++ PPMGroovyScripts/src/CreateProjectStructure.groovy
@@ -1,4 +1,4 @@
-import java.nio.file.Path
+
 
 //test values
 String projectRoot = "D:/dev/projects"; //eventually. get from PPM env
@@ -31,6 +31,62 @@ if (!fileDiscription.exists()){
 
 //Copy Template Files
 
-java.nio.file.Path source = templateDir;
-java.nio.file.Path dest = projectRoot + "/" + projectName + "/Documents";
-java.nio.file.Files.copy(source, dest);
+File src = new File(templateDir); 
+File dest = new File(projectRoot + "/" + projectName + "/Documents");
+
+try {
+	copyFolder(src, dest);
+} catch (Exception e) {
+	print "Templates not copied";
+	e.printStackTrace();
+}
+
+//////////////////////Classes//////////////////////////
+
+
+private void copyFolder(File src, File dest)
+throws IOException{
+	
+	
+
+if(src.isDirectory()){
+
+	//if directory not exists, create it
+	if(!dest.exists()){
+	   dest.mkdir();
+	   System.out.println("Directory copied from "
+					  + src + "  to " + dest);
+	}
+
+	//list all the directory contents
+	def files = src.list();
+
+	for (String file : files) {
+	   //construct the src and dest file structure
+	   File srcFile = new File(src, file);
+	   File destFile = new File(dest, file);
+	   //recursive copy
+	   copyFolder(srcFile,destFile);
+	}
+	
+} else {
+    		// file, then copy it
+    		//Use bytes stream to support all file types
+    		InputStream ina = new FileInputStream(src);
+    	        OutputStream out = new FileOutputStream(dest); 
+ 
+    	        byte[] buffer = new byte[1024];
+ 
+    	        int length;
+    	        //copy the file content in bytes 
+    	        while ((length = ina.read(buffer)) > 0){
+    	    	   out.write(buffer, 0, length);
+    	        }
+ 
+    	        ina.close();
+    	        out.close();
+    	        System.out.println("File copied from " + src + " to " + dest);
+    	}
+	
+
+}
---
+++ PPMGroovyScripts/src/PopulateIntakeDoc.groovy
@@ -0,0 +1,50 @@
+import word.utils.Utils;
+import word.w2004.elements.Table
+import word.w2004.elements.tableElements.TableEle;
+import word.utils.TestUtils;
+//test values
+String projectRoot = "D:/dev/projects"; //eventually. get from PPM env
+String templateDir = "D:/dev/templates"; //eventually get from PPM
+
+String projectName = "Application_Test-11.3-W7-x64"; //get from PPM env
+String projectDiscription = "Application for doing work";
+String projectIntakeDocName = "ppmIntake.doc";
+
+
+/////////////////////////////////////////////////////
+// Documents must be saved as word 2003 xml format //
+/////////////////////////////////////////////////////
+
+def projectDocumentFolder =  projectRoot + "/" + projectName + "/Documents";
+
+//modify document
+
+ xmlTemplate = Utils.readFile(projectDocumentFolder + "/" + projectIntakeDocName);
+ 
+ Table tbl = new Table();
+ tbl.addTableEle(TableEle.TH, "Jira Number", "Description"); 
+ 
+ xmlTemplate = replacePh(xmlTemplate, "ppmAppName", projectName);
+ xmlTemplate = replacePh(xmlTemplate, "ppmAppManufacturer", projectDiscription);
+ xmlTemplate = replacePh(xmlTemplate, "ppmAppVersion", projectIntakeDocName);
+ 
+ 
+ 
+//Save Document
+
+ newDoc = new File(projectDocumentFolder + "/n" + projectIntakeDocName); 
+ 
+ newDoc << xmlTemplate
+ 
+////////////////Subs///////////////
+
+ private String replacePh(String base, String placeHolder, String value) {
+     if(!base.contains(placeHolder)) {
+         //don't want to use log now because I want to keep it simple...
+         System.out.println("### WARN: couldn't find the place holder: " + placeHolder);
+         return base;
+     }
+     return base.replace(placeHolder, value);
+ }
+ 
+ 
